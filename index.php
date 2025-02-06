6047ef903db114a0fcd2cdb83d15e61b57d90c8e
manuel795 <manuelmarintrejo@gmail.com>
2025-02-06 09:42:55 -0600

    alineacioncodigo

--- index.php
+++ index.php
@@ -4,14 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bienvenid@ SUTGESE</title>
-   <link rel="stylesheet" href="css/bootstrap.min.css">
-   <link rel="stylesheet" href="css/luces-animacion.css">
-   <link rel="stylesheet" href="css/plantillaG.css">
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-   <style>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/luces-animacion.css">
+    <link rel="stylesheet" href="css/plantillaG.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+        <style>
   /* Estilos para el modal */
 
-   </style>
+    </style>
 <header>
         <div class="encabezado">
             <div class="titulog">
